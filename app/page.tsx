ce9a6a0edb83a9ef7c173249579f2695d0d99fa9
jo <jyothishkpk@gmail.com>
2025-05-29 19:16:14 +0530

    trtrtr

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
 import Header from './components/Header';
 import Footer from './components/Footer';
 import BannerCarousel from './components/BannerCarousel';
+import InteractiveHomeSection from './components/InteractiveHomeSection';
 
 export default function Home() {
   return (
     <div className="ba-wrapper">
     <Header />
     <BannerCarousel />
+    <InteractiveHomeSection />
     <Footer />
     </div>
   );
