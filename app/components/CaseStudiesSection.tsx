ccdf4cac89deacb02f25577aa7f4c56fff136a7d
jo <jyothishkpk@gmail.com>
2025-06-04 11:56:52 +0530

    erro fixed

--- app/components/CaseStudiesSection.tsx
+++ app/components/CaseStudiesSection.tsx
@@ -2,7 +2,6 @@
 import { useEffect, useRef } from "react";
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
-import Link from "next/link";
 
 gsap.registerPlugin(ScrollTrigger);
 
